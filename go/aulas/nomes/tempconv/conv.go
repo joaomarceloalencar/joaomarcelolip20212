dd51fe3512beb78d1e607f87ac53c775da78ae80
joaomarceloalencar <joao.marcelo@ufc.br>
2021-11-30 14:55:39 -0300

    Pacote de exemplo para importações.

---
+++ go/aulas/nomes/tempconv/conv.go
@@ -0,0 +1,6 @@
+package tempconv
+
+// CToF converte uma temperatura em Celsius para Fahrenheit
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32)}
+// FToC converte uma temperatura em Fahrenheit para Celsius
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9)}
---
+++ go/aulas/nomes/tempconv/tempconv.go
@@ -0,0 +1,15 @@
+// Pacote tempconv realiza conversões mde Celsius a Fahrenheit.
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC 	     Celsius = 0
+	BoilingC          Celsius = 100
+)
+
+
