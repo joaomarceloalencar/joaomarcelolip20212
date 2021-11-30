3acef9a977b831e59c1a2bb7707c56ed598eac98
joaomarceloalencar <joao.marcelo@ufc.br>
2021-11-30 19:26:20 -0300

    Limpando para fazer da maneira certa.

--- go/aulas/nomes/cf.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-   "fmt"
-   "aulas/nomes/tempconv"
-)
-
-func main() {
-   fmt.Println(tempconf.CToF(10))
-}
--- go/aulas/nomes/tempconv/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/joaomarceloalencar/joaomarcelolip20212/go/aulas/nomes/tempconv
-
-go 1.13
