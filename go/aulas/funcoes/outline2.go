38145f738a58b051f36819be10ac005f765ef436
joaomarceloalencar <joao.marcelo@ufc.br>
2022-01-29 11:37:52 -0300

    Exemplo de Funções de Alta Ordem

---
+++ go/aulas/funcoes/outline2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"golang.org/x/net/html"
+)
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth * 2, "", n.Data)
+		depth++
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth * 2, "", n.Data)
+		depth--
+	}
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+
+	forEachNode(doc, startElement, endElement)
+}
+
